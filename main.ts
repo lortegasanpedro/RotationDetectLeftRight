989adf65108bd23f2d9120ca4b0ad936a02fdc19
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-04-08 20:15:23 +0200

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 let pitch = 0
 let roll = 0
 let rollAbs = 0
+let x = 0
+let y = 0
+let z = 0
 basic.forever(function () {
     pitch = input.rotation(Rotation.Pitch)
     roll = input.rotation(Rotation.Roll)
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    z = input.acceleration(Dimension.Z)
     //rollAbs = Math.abs(roll)
     /*if (roll < 40 && roll > -40) {
         basic.showLeds(`
@@ -25,6 +31,9 @@ basic.forever(function () {
     // serial.writeLine("Pitch: " + pitch)
     serial.writeLine("Pitch: " + pitch)
     serial.writeLine("Roll: " + roll)
+    serial.writeLine("X: " + x)
+    serial.writeLine("Y: " + y)
+    serial.writeLine("Z: " + z)
     //serial.writeLine("rollAbs: " + rollAbs)
     basic.pause(30)
 })
