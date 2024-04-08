9b0ef69b1fe1dc9c29c2646d756167a60a0fbfff
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-04-08 19:47:15 +0200

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ let pitch = 0
 let roll = 0
 let rollAbs = 0
 basic.forever(function () {
+    pitch = input.rotation(Rotation.Pitch)
     roll = input.rotation(Rotation.Roll)
     //rollAbs = Math.abs(roll)
-    if (roll < 40 && roll > -40) {
+    /*if (roll < 40 && roll > -40) {
         basic.showLeds(`
             . # . # .
             . . . . .
@@ -20,9 +21,10 @@ basic.forever(function () {
             . # . # .
             # . . . #
             `)
-    }
+    }*/
     // serial.writeLine("Pitch: " + pitch)
+    serial.writeLine("Pitch: " + pitch)
     serial.writeLine("Roll: " + roll)
     //serial.writeLine("rollAbs: " + rollAbs)
-    basic.pause(200)
+    basic.pause(30)
 })
