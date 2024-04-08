619853d315ea7aefb19b9fc3a799451c2cb480a9
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-04-08 22:47:50 +0200

    Actualizar main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-let pitch = 0
-let roll = 0
-let rollAbs = 0
 let x = 0
-let y = 0
 let z = 0
+let y = 0
+let rollAbs = 0
+let roll = 0
+let pitch = 0
 basic.forever(function () {
-    //pitch = input.rotation(Rotation.Pitch)
-    //roll = input.rotation(Rotation.Roll)
+    // pitch = input.rotation(Rotation.Pitch)
+    // roll = input.rotation(Rotation.Roll)
     x = input.acceleration(Dimension.X)
-    //y = input.acceleration(Dimension.Y)
-    //z = input.acceleration(Dimension.Z)
-    //rollAbs = Math.abs(roll)
-    if (x > 40 && x < -40) {
+    // y = input.acceleration(Dimension.Y)
+    // z = input.acceleration(Dimension.Z)
+    // rollAbs = Math.abs(roll)
+    if (x < 300 && x > -300) {
         basic.showLeds(`
             . # . # .
             . . . . .
@@ -19,29 +19,29 @@ basic.forever(function () {
             # . . . #
             . # # # .
             `)
-    } else if (x > 41 && x < -1023)  {
+    } else if (x > 301 && x < 1023) {
         basic.showLeds(`
-            # . . . #
-            . # . # .
             . . # . .
-            . # . # .
-            # . . . #
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
             `)
-    } else {
+    } else if (x > -301 && x < -1023) {
         basic.showLeds(`
-            . . . . .
-            . # . # .
             . . # . .
-            . # . # .
-            . . . . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
             `)
     }
     // serial.writeLine("Pitch: " + pitch)
-    //serial.writeLine("Pitch: " + pitch)
-    //serial.writeLine("Roll: " + roll)
+    // serial.writeLine("Pitch: " + pitch)
+    // serial.writeLine("Roll: " + roll)
     serial.writeLine("X: " + x)
-    //serial.writeLine("Y: " + y)
-    //serial.writeLine("Z: " + z)
-    //serial.writeLine("rollAbs: " + rollAbs)
+    // serial.writeLine("Y: " + y)
+    // serial.writeLine("Z: " + z)
+    // serial.writeLine("rollAbs: " + rollAbs)
     basic.pause(30)
 })
